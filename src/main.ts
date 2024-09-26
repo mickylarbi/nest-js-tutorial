e46fcd09c033bdf290c5a14ac7ad4f8bd87c22b8
Michael Larbi <mickylarbi28@gmail.com>
2024-09-26 07:55:35 +0000

    change logging to specific controllers

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bufferLogs: true
-  });
-  app.useLogger(app.get(MyLoggerService))
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
